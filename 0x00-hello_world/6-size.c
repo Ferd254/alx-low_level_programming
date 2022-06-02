2d5da9d021eda68b8985f19caafa51a570710a3a
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-02 16:21:57 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,6 +18,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof (d));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(e));
 	printf("Size of a float: %lu byte(s)\n", sizeof(c));
-
 	return (0);
 }
