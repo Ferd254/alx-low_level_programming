f6050e0d427895113f901e104d3655a884dfba52
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-02 14:05:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - Prints a sentence
+ *
+ *   (*
+ *
+ *    *Description: Uses printf() to print a sentence)?
+ *
+ *     *Return: Zero
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
