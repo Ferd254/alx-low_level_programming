43b95d3f53b29b7ba2d8abd53c0ee466476f9aac
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-02 14:38:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,14 @@
 
 /**
  * main - Prints a sentence
- (*
+ *
  * Description:Uses the funtion put to print a line)?
  * Return Always 0 (Success)
  */
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+		
 }
