c8cdd3d6eb0c32fdb6e61511c6e017c8770e7cd7
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-03 16:13:31 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase,uppercase and newline
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char m;
+	char M;
+
+	for (m = 'a' ; m <= 'z' ; m++)
+	{
+	 	putchar (m);
+	}
+
+	for (M = 'A' ; M <= 'Z' ; M++)
+	{
+		putchar (M);
+	}
+	
+	putchar ('\n');
+
+	return (0);
+}	
