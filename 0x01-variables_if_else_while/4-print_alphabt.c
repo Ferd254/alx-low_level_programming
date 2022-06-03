a57756796e861799dead95e13d320b8496a19c96
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-03 16:53:13 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase except q, e
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char m;
+
+	for (m = 'a' ; ch <= 'z' ; m++)
+	{
+		if (m != 'e' && m != 'q')
+		{
+			putchar (m);
+		}
+
+	}
+	putchar ('\n');
+
+	return (0);
+}
