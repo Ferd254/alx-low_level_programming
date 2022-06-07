16115a6611c5521bfd56256f205d3e7ee76b7de3
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 14:05:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,17 +11,17 @@
 
 int main(void)
 {
-	char c [8] = "_putchar";
+	char c[8] = "_putchar";
 	int i;
 
-	for(i = 0 ; i <= 8; i++)
+	for (i = 0 ; i < 8; i++)
 	{
 		putchar(c[i]);
 	}
 	{
 		putchar('\n');
 	}
-		return(0);
+		return (0);
 }
 
 
