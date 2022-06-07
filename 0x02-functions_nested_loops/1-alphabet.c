b3541b0a50c67f0dd9519813f2a49ddc4c534462
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 14:17:00 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n')
+	}
+		return (0);
+
+}
