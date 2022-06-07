af34433934c5585afee3c962b9d3e222314c08c4
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 14:21:20 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  *
  * Description - prints the alphabet, in lowercase, followed by a new line
  *
- * Return: 0
+ * Return: void
  */
 
 void print_alphabet(void)
@@ -18,8 +18,8 @@ void print_alphabet(void)
 		putchar(c);
 	}
 	{
-		putchar('\n')
+		putchar('\n');
 	}
-		return (0);
+
 
 }
