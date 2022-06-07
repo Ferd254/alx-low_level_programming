415abc95c164709119fd64646aac521baa3ed972
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 15:51:06 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - Prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
  *
  * Description: Prints 10 times the alphabet in lower case
  *
