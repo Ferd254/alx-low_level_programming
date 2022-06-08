4c8f8710d14ea561eef5e37fcfffe009ebe185d5
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 18:49:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,14 +15,14 @@ int print_last_digit(int n)
 	int l;
 
 	if (n < 0)
-	{
+	
 		n = n * -1;
 		l = n % 10;
 		_putchar(l + '0');
-	} else
-	{	l = n % 10;
+       	else
+		l = n % 10;
 		_putchar(1 + '0');
-	}
+
 	return (1);
 }
 
