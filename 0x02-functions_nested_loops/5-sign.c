d3f251c340b365a41bcaaa30f444e77e5263aa40
Ferd254 <ferdinardwaguma@gmail.com>
2022-06-07 17:26:47 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -19,17 +19,17 @@ int print_sign(int n)
 		if (n > 0)
 		{
 			result = 1;
-			putchar('+');
+			_putchar('+');
 		} else if (n < 0)
 		{
 			result = -1;
-			putchar('-');
+			_putchar('-');
 		}
 
 		else
 		{
 			result = 0;
-			putchar('0');
+			_putchar('0');
 		}
 
 		return (result);
